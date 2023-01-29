1a07386ca65c2cd4377b6bdd721803958cebc498
Aldy Azarya <aldy1612@gmail.com>
2023-01-29 18:05:47 +0700

    setting gsc

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,6 +8,7 @@ export default function Document() {
           href="https://api.fontshare.com/v2/css?f[]=satoshi@900,700,500,301,701,300,501,401,901,400&display=swap"
           rel="stylesheet"
         />
+        <meta name="google-site-verification" content="nEHYtKLe1ttFitTA5xj-4UwhG4woFUybub9Bhxwipz4" />
       </Head>
       <body className="bg-white transition-colors dark:bg-dark dark:text-white">
         <Main />
