67088ec00f42f279bee5eb5baf878d9e42416253
Aldy Azarya <aldy1612@gmail.com>
2023-01-29 18:02:52 +0700

    setting gtm and ga

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,7 +5,12 @@ import nProgress from "nprogress";
 import { ThemeProvider } from "next-themes";
 import { SWRConfig } from "swr";
 
-import 'react-tippy/dist/tippy.css';
+import Script from "next/script";
+
+import TagManager from "react-gtm-module";
+import { useEffect } from "react";
+
+import "react-tippy/dist/tippy.css";
 import "@/styles/globals.css";
 import "@/styles/dracula.css";
 
@@ -13,7 +18,11 @@ Router.events.on("routeChangeStart", nProgress.start);
 Router.events.on("routeChangeError", nProgress.done);
 Router.events.on("routeChangeComplete", nProgress.done);
 
- function App({ Component, pageProps }: AppProps) {
+function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    TagManager.initialize({ gtmId: "GTM-PBG5TX4" });
+  }, []);
+
   return (
     <ThemeProvider attribute="class" defaultTheme="dark" enableSystem={false}>
       <SWRConfig
@@ -21,10 +30,23 @@ Router.events.on("routeChangeComplete", nProgress.done);
           fetcher: (url) => axios.get(url).then((res) => res.data),
         }}
       >
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-9R45TJTLYG"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-9R45TJTLYG');
+        `}
+        </Script>
         <Component {...pageProps} />
       </SWRConfig>
     </ThemeProvider>
   );
 }
 
-export default App
+export default App;
